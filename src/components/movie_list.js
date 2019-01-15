a29be398ea404ba05aa4ef3744ef86f9f9b839ff
rojrun <runroj@gmail.com>
2019-01-14 17:00:40 -0800

    Added movie quote component

--- src/components/movie_list.js
+++ src/components/movie_list.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react';
 
 class MovieList extends Component {
+    isPrivate=  this.props.match.params.type === 'private'
+
     render() {
         return(
             <div>
-                <h1 className="center">Movie List</h1>
+                <h1 className="center">
+                    <small className="grey-text">{this.isPrivate && 'Private '}</small>
+                    Movie List
+                </h1>
             </div>
         );
     }
