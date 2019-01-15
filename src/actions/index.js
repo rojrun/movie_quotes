97acd8007af85b153128c83a61bb370b3e673ca5
rojrun <runroj@gmail.com>
2019-01-14 18:25:24 -0800

    Added user auth

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,14 @@
 import types from './types';
+
+
+export function signIn(){
+    return {
+        type: types.SIGN_IN
+    };
+}
+
+export function signOut(){
+    return {
+        type: types.SIGN_OUT
+    }
+}
